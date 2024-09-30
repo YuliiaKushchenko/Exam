2c8bc5f82c115e3f9416f50880c92d57418451f0
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-09-30 18:37:29 +0200

    test for checking menu items and url was added

--- src/main/java/pages/elements/HeaderElements.java
+++ src/main/java/pages/elements/HeaderElements.java
@@ -1,10 +1,15 @@
 package pages.elements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.CommonActionsWithElements;
 import pages.LoginPage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HeaderElements extends CommonActionsWithElements {
 
@@ -15,8 +20,30 @@ public class HeaderElements extends CommonActionsWithElements {
         super(webDriver);
     }
 
+
     public LoginPage clickOnAuthorizationIcon() {
         clickOnElement(authorizationIcon);
         return new LoginPage(webDriver);
+    }
+
+    public void checkMenuItems() {
+        List<String> menuItems = Arrays.asList(
+                "/pro-nas/",
+                "/dostavka-i-oplata/",
+                "/programa-loyalnosti/",
+                "/blog/",
+                "/vidguki/",
+                "/kontakti/");
+
+        waitUntilPageIsLoaded();
+        menuItems.forEach(
+                (String menuItem) -> {
+                    String menuItemLocator = String.format("//div[@class='top-holder']//a[@href='https://dermal-cosmetics.com.ua%s']", menuItem);
+                    WebElement tab = webDriver.findElement(By.xpath(menuItemLocator));
+                    ((JavascriptExecutor) webDriver).executeScript("arguments[0].click();", tab);
+                    waitUntilPageIsLoaded();
+                    checkItemUrl(menuItem);
+                }
+        );
     }
 }
